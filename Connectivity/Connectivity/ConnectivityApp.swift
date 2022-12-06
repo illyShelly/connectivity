a158770cae9e7006e31ace708efb2f213f548dd8
Ilona Sellenberk <ilona.sell@gmail.com>
2022-12-06 13:35:41 +0100

    add project, update .gitignore

---
+++ Connectivity/Connectivity/ConnectivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectivityApp.swift
+//  Connectivity
+//
+//  Created by Ilona Sellenberkova on 06/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ConnectivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
